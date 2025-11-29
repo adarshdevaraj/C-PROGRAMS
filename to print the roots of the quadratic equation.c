0f85d1f3d16289fb8be1406d4a9b4c40e9d83ee8
adarshdevaraj <adarshdevaraj661@gmail.com>
2025-11-29 16:52:17 +0530

    Add files via upload

---
+++ to print the roots of the quadratic equation.c
@@ -0,0 +1,35 @@
+/*Write a C program to print the roots of quadratic equation*/
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a,b,c,d;
+	float r1,r2,real,imaginary;
+	printf("Enter any three integer numbers");
+	scanf("%d%d%d",&a,&b,&c);
+	d=b*b-4*a*c;
+	
+	if(d>0)
+	{
+		printf("/n roots are real");
+		r1=(-b+sqrt(d))/2*a ;
+		r2=(-b-sqrt(d))/2*a ;
+		printf("r1=%f,r2=%f",r1,r2);
+		
+	}
+	else if(d==0)
+	{
+		printf("/n Roots are equal");
+		r1=-b/2*a;
+		r2=-b/2*a;
+		printf("r1=%f,r2=%f",r1,r2);
+	}
+	else
+	{
+		printf("/n Roots are imainary");
+		real=-b/2*a;
+		imaginary=sqrt(d)/2*a;
+		printf("r1=%f+%fi,r2=%f-%fi",real,imaginary,real,imaginary);
+		
+	}
+}
